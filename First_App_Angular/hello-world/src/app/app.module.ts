8202c6ee08a9a09929be85ac84248bb67783bd37
G Emmanuel <emmanuel012k@gmail.com>
2023-03-21 16:41:46 +0530

    Intial commit
    Intial commit

---
+++ First_App_Angular/hello-world/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,AboutComponent   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
